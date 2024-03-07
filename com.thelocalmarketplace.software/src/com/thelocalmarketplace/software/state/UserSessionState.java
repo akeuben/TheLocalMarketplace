aad421767278eb28b5bc7209f0828ceb79fbd7a1
Avery Keuben <avery1516@gmail.com>
2024-03-07 12:27:38 -0700

    Add initial enum

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/state/UserSessionState.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/state/UserSessionState.java
@@ -1,5 +1,25 @@
 package com.thelocalmarketplace.software.state;
 
 public enum UserSessionState {
+	/**
+	 * This state is used when the system
+	 * is ready for an item to be added to the bagging area.
+	 */
+	READY_FOR_ITEM(null),
+	/**
+	 * This state is used when the system
+	 * is waiting for an item to be added to the bagging area
+	 */
+	WAITING_FOR_BAGGING(null),
+	/**
+	 * This state is used when the system
+	 * is ready to accept payment
+	 */
+	READY_FOR_PAYMENT(null);
 	
+	private Object state;
+	
+	private UserSessionState(Object sessionState) {
+		this.state = sessionState;
+	}
 }
