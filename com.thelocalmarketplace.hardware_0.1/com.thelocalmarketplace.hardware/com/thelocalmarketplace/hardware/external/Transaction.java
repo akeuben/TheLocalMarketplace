1286b0e13d5aeebe712e6a5a9e19bd9e8fa8b087
Joey Fisher <joey.fisher@ucalgary.ca>
2024-03-07 15:05:18 -0700

    Added skeleton for Transaction.java and created addItem() method

--- com.thelocalmarketplace.hardware_0.1/com.thelocalmarketplace.hardware/com/thelocalmarketplace/hardware/external/Transaction.java
+++ com.thelocalmarketplace.hardware_0.1/com.thelocalmarketplace.hardware/com/thelocalmarketplace/hardware/external/Transaction.java
@@ -1,5 +1,27 @@
+import com.thelocalmarketplace.hardware.BarcodedProduct;
+import java.util.ArrayList;
+import com.jjjwelectronics.scanner.Barcode;
+import static com.thelocalmarketplace.hardware.external.ProductDatabases.BARCODED_PRODUCT_DATABASE;
+
 public class Transaction {
 
+    /**
+     * Items contained in an instance of transaction TODO Create constructor
+     */
+    public static final ArrayList<BarcodedProduct> products = new ArrayList<>();
 
+    /**
+     * Adds product to transaction array list
+     * @param barcode
+     */
+    public static void addItem(Barcode barcode) {
+        if (barcode != null) {
+            BarcodedProduct product = BARCODED_PRODUCT_DATABASE.get(barcode);
+            products.add(product);
+        }
+        else {
+            throw new NullPointerException("barcode");
+        }
 
+    }
 }
