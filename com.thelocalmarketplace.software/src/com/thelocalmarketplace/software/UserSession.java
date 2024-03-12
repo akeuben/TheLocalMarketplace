73f725acadca88a8e29ee8e5223e0b33cc8de73b
Zep <connor.uni.calg@gmail.com>
2024-03-12 11:28:30 -0600

    prepare user session for the merging of the transaction class

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/UserSession.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/UserSession.java
@@ -3,5 +3,5 @@ import com.thelocalmarketplace.software.state.UserSessionState;
 public class UserSession
 {
     public UserSessionState state;
-    //will be for transation idk
+    //Transaction variable here when merged
 }
