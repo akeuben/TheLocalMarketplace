90f5020c2d7f89a0fe0d822c1f753fef78322e32
Nathan Vaters <nathanvaters@Nathans-MacBook-Air.local>
2024-03-14 19:22:02 -0600

    Add implementation for WaitingForBaggingState

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/state/WaitingforBaggingState.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/state/WaitingforBaggingState.java
@@ -5,36 +5,47 @@ import java.math.BigDecimal;
 import com.jjjwelectronics.Mass;
 import com.jjjwelectronics.scanner.Barcode;
 import com.tdc.coin.Coin;
+import com.thelocalmarketplace.software.payment.Transaction;
 
 public class WaitingforBaggingState implements IUserSessionState<UserSessionState> {
 
 	@Override
 	public UserSessionState onStateSet() {
-		return null; 
+		System.out.println("Waiting for item to be bagged..."); // Prompt the user to bag their item
+		return null; // State has not changed, so return null
 	}
 
 	@Override
-	public void onStateUnset() {
-		// TODO Auto-generated method stub
-		
-	}
+	public void onStateUnset() throws RuntimeException{}
 
+	/**The customer should not be able to scan a new item until 
+	 * they have placed the previously scanned item in the bagging area.
+	 * If the customer scans an item when the self-checkout is in WaitingForBaggingState,
+	 * it will not be added to the transaction, and the customer will be
+	 * prompted to put the previously scanned item in the bagging area, otherwise
+	 * they will not be able to continue their session as the self-checkout will remain
+	 * in the WaitingForBaggingState.
+	 */
+	
 	@Override
 	public UserSessionState onScanBarcode(Barcode barcode) {
-		// TODO Auto-generated method stub
+		System.out.println("Place previously scanned item in bagging area before scanning next item"); 
 		return null;
 	}
 
 	@Override
 	public UserSessionState onWeightChanged(Mass mass) {
-		// TODO Auto-generated method stub
+		// Case 1: weight was removed
+		// Case 2: improper weight was added
+		// Case 3: proper weight was added
 		return null;
 	}
 
 	@Override
 	public UserSessionState onCoinInserted(BigDecimal value) {
-		// TODO Auto-generated method stub
+		System.out.println("Not ready for payment, please place item in bagging area"); // Do not accept payment from...
+		//...customer when an item is not yet placed in the bagging area
+		// Spit coin out
 		return null;
 	}
-
 }
