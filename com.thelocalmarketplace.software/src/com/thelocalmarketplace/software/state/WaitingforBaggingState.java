84b5deaabd4732a285dfc9aea2cb9b2ef8fbab14
Nathan Vaters <nathanvaters@gmail.com>
2024-03-14 20:04:01 -0600

    Add implementation for WaitingForBaggingState (did not save all changes in previous commit

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/state/WaitingforBaggingState.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/state/WaitingforBaggingState.java
@@ -1,51 +1,69 @@
 package com.thelocalmarketplace.software.state;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 import com.jjjwelectronics.Mass;
+import com.jjjwelectronics.OverloadedDevice;
 import com.jjjwelectronics.scanner.Barcode;
-import com.tdc.coin.Coin;
+import com.thelocalmarketplace.software.SelfCheckout;
+import com.thelocalmarketplace.software.feature.ElectronicScaleFeature;
 import com.thelocalmarketplace.software.payment.Transaction;
 
 public class WaitingforBaggingState implements IUserSessionState<UserSessionState> {
+	
+	private final Mass MAXIMUM_MASS_DIFFERENCE = new Mass(BigInteger.valueOf(10*Mass.MICROGRAMS_PER_GRAM)); // Maximum weight discrepancy allowed
 
 	@Override
 	public UserSessionState onStateSet() {
-		System.out.println("Waiting for item to be bagged..."); // Prompt the user to bag their item
-		return null; // State has not changed, so return null
+		
+		if(!SelfCheckout.getInstance().supportsFeature(ElectronicScaleFeature.class)){ // If the self-checkout does not have an electronic scale,
+			return UserSessionState.READY_FOR_ITEM;									   // skip this state and go back to ReadyForItemState
+		}
+		
+		Mass mass;
+		try {
+			mass = SelfCheckout.getInstance().getFeature(ElectronicScaleFeature.class).getScale().getCurrentMassOnTheScale();
+		} catch (OverloadedDevice | RuntimeException e) { // Catch errors when retrieving mass from scale
+			return null;
+		}
+		
+		// Item is too light to initiate a mass change
+		Transaction currentTransaction = SelfCheckout.getInstance().getCurrentSession().getTransaction(); // Get current transaction
+		Mass expectedMass = currentTransaction.getExpectedMass(); // Get expected mass
+		Mass absoluteDifference = expectedMass.difference(mass).abs(); // Compare expected and actual mass of item placed in bagging area
+		
+		if(absoluteDifference.compareTo(MAXIMUM_MASS_DIFFERENCE) == -1) { // If item falls within the scale's sensitivity window,
+			return UserSessionState.READY_FOR_ITEM; 					  // go back to ReadyForItemState
+		}
+			
+		return null; // If item is not the correct weight, weight for correct item to be placed/removed from bagging area
+		
 	}
 
 	@Override
 	public void onStateUnset() throws RuntimeException{}
-
-	/**The customer should not be able to scan a new item until 
-	 * they have placed the previously scanned item in the bagging area.
-	 * If the customer scans an item when the self-checkout is in WaitingForBaggingState,
-	 * it will not be added to the transaction, and the customer will be
-	 * prompted to put the previously scanned item in the bagging area, otherwise
-	 * they will not be able to continue their session as the self-checkout will remain
-	 * in the WaitingForBaggingState.
-	 */
 	
 	@Override
 	public UserSessionState onScanBarcode(Barcode barcode) {
-		System.out.println("Place previously scanned item in bagging area before scanning next item"); 
-		return null;
+		return null; // State does not change if user scans an additional item
 	}
-
+	
 	@Override
 	public UserSessionState onWeightChanged(Mass mass) {
-		// Case 1: weight was removed
-		// Case 2: improper weight was added
-		// Case 3: proper weight was added
-		return null;
+		Transaction currentTransaction = SelfCheckout.getInstance().getCurrentSession().getTransaction(); // Get current transaction
+		Mass expectedMass = currentTransaction.getExpectedMass(); // Get expected mass
+		Mass absoluteDifference = expectedMass.difference(mass).abs(); // Compare expected and actual mass of item placed in bagging area
+		
+		if(absoluteDifference.compareTo(MAXIMUM_MASS_DIFFERENCE) == -1) { // If item falls within the scale's sensitivity window,
+			return UserSessionState.READY_FOR_ITEM; 					  // go back to ReadyForItemState
+		}
+			
+		return null; // If item is not the correct weight, weight for correct item to be placed/removed from bagging area
 	}
-
+	
 	@Override
 	public UserSessionState onCoinInserted(BigDecimal value) {
-		System.out.println("Not ready for payment, please place item in bagging area"); // Do not accept payment from...
-		//...customer when an item is not yet placed in the bagging area
-		// Spit coin out
-		return null;
+		return null; // State does not change if user inserts coin
 	}
 }
