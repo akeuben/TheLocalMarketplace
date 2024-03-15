e51d7c1f3b8df14cf50217be55c62bef876a30bc
Avery Keuben <avery1516@gmail.com>
2024-03-15 01:08:39 -0600

    Fix formatting issues

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/state/ReadyForPaymentState.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/state/ReadyForPaymentState.java
@@ -12,17 +12,17 @@ public class ReadyForPaymentState implements IUserSessionState<UserSessionState>
 
 	@Override
 	public UserSessionState onStateSet() {
-		
 		//Get current balance by creating a transaction instance
 		Transaction transaction = SelfCheckout.getInstance().getCurrentSession().getTransaction(); 
+		
 		//Check if balance is 0 and that there is an item to end session 
 		if (transaction.getTotalCost().compareTo(BigDecimal.ZERO) <= 0 && transaction.getProducts().length > 0) {
-		        SelfCheckout.getInstance().endCurrentSession(); 
-		    }
-		//If item is at a 0, set state to ready for item
-		    else if (transaction.getProducts().length == 0) {
-		    	return UserSessionState.READY_FOR_ITEM;
-		    }
+			SelfCheckout.getInstance().endCurrentSession(); 
+		} else if (transaction.getProducts().length == 0) {
+			//If item is at a 0, set state to ready for item
+	    	return UserSessionState.READY_FOR_ITEM;
+	    }
+		
 		// Enable the coin slot to allow the user to insert a coin while the software
 		// is in the correct state
 		SelfCheckout.getInstance().getHardware().coinSlot.enable();
