ab43e6051fd4e3849c788540fd39345e1e4c525d
starboy588 <149621131+starboy588@users.noreply.github.com>
2024-03-20 18:51:12 -0600

    update to 'change' class
    returnChange class and adding imports

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/payment/Change.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/payment/Change.java
@@ -1,5 +1,21 @@
 package com.thelocalmarketplace.software.payment;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import com.tdc.*;
+
 public class Change {
 
+	public BigDecimal returnChange() {
+		
+		
+		
+		return null;
+		
+	}
 }
