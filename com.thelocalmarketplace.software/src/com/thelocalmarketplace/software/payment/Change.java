bbba26c61c2857303415dfbee3f01fd84809b0f7
suleman <83623379+sulemanbasit@users.noreply.github.com>
2024-03-21 16:16:53 -0600

    Create calculate change function

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/payment/Change.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/payment/Change.java
@@ -9,14 +9,35 @@ import java.util.List;
 import java.util.Map;
 
 import com.tdc.*;
+import com.tdc.banknote.AbstractBanknoteDispenser;
+import com.tdc.coin.AbstractCoinDispenser;
+import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
+
+import ca.ucalgary.seng300.simulation.SimulationException; 
 
 public class Change {
+	/* Global variables */
+//	private boolean banknotePause = false;
+//	private boolean debug = false;
+//	private boolean coinPause = false;
+//	private AbstractSelfCheckoutStation station;
+	private ArrayList<BigDecimal> coinsToReturn = new ArrayList<BigDecimal>();
+	private ArrayList<Integer> banknotesToReturn = new ArrayList<Integer>();
+	private Map<BigDecimal, AbstractCoinDispenser> coinDispensers;
+	private Map<Integer, AbstractBanknoteDispenser> banknoteDispensers;
+	private int[] banknoteDenominations;
+	private List<BigDecimal> coinDenominations;
+	
 
 	public BigDecimal returnChange(Currency currency) {
 		
 		
 		
 		return BigDecimal.ZERO;
+		
+	}
+	
+	public void calculateChange(double totalChange) throws OverloadException{
 		
 	}
 }
