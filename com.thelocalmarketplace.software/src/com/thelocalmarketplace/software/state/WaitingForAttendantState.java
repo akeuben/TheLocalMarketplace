85ab8c953513c6dcbd1b33dfa850ce58b3c13b31
Joey - School <joey.fisher@ucalgar.ca>
2024-03-22 11:23:16 -0600

    Created WaitingForAttendantState

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/state/WaitingForAttendantState.java
@@ -0,0 +1,33 @@
+package com.thelocalmarketplace.software.state;
+
+import java.math.BigDecimal;
+
+public class WaitingForAttendantState implements IUserSessionState<UserSessionState> { {
+
+}
+
+    @Override
+    public UserSessionState onStateSet() {
+        return null;
+    }
+
+    @Override
+    public void onStateUnset() {
+
+    }
+
+    @Override
+    public UserSessionState onScanBarcode(Barcode barcode) {
+        return null;
+    }
+
+    @Override
+    public UserSessionState onWeightChanged(Mass mass) {
+        return null;
+    }
+
+    @Override
+    public UserSessionState onCoinInserted(BigDecimal value) {
+        return null;
+    }
+}
