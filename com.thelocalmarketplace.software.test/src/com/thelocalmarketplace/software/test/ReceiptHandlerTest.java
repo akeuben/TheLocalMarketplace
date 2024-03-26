a2cd4a18b992df03805591148a053fa528fa5053
Nathan Vaters <nathanvaters@gmail.com>
2024-03-25 18:10:01 -0600

    Add ReceiptHandlerTest class.

---
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/ReceiptHandlerTest.java
@@ -0,0 +1,73 @@
+package com.thelocalmarketplace.software.test;
+import com.thelocalmarketplace.software.session.ReceiptPrinterHandler;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * SENG 300 Project - Group 1:
+ * 
+ * Avery Keuben - 30170731
+ * Moiz Siddiqui - 30150291
+ * Ammaar Melethil - 30141956
+ * Joey Fisher - 30105628
+ * Ethan Pangilinan - 30179143
+ * Joshua Kraft - 30171525
+ * Nathan Vaters - 30121908
+ * Max Butcher - 30149202
+ * Neeraj Ghansela - 30157473
+ * Ansel Sulejmani - 30178521
+ * Suleman Basit - 30132816
+ * Jacob Boyden - 30193220
+ * Cheshta Sharma - 30064538
+ * Callum Bates - 30188601
+ * Armughan Mustafa - 30154601
+ * Connor Ell - 30073291
+ * Saif Farag - 30195046
+ * Ivan Agalakov - 30172107
+ * Samuel Turner - 10064857
+ * Stephanie Sevilla - 30176781
+ * Winston Wang - ????????
+ */
+
+public class ReceiptHandlerTest {
+
+    @Test
+    public void testRefillFlagsSetWhenNeitherFlagSet() {
+        ReceiptPrinterHandler printerHandler = new ReceiptPrinterHandler(null);
+        assertFalse(printerHandler.refillFlagsSet());
+    }
+
+    @Test
+    public void testRefillFlagsSetWhenInkFlagSet() {
+        ReceiptPrinterHandler printerHandler = new ReceiptPrinterHandler(null);
+        printerHandler.thePrinterIsOutOfInk();
+        assertTrue(printerHandler.refillFlagsSet());
+    }
+
+    @Test
+    public void testRefillFlagsSetWhenPaperFlagSet() {
+        ReceiptPrinterHandler printerHandler = new ReceiptPrinterHandler(null);
+        printerHandler.thePrinterIsOutOfPaper();
+        assertTrue(printerHandler.refillFlagsSet());
+    }
+
+    @Test
+    public void testRefillFlagsSetWhenBothFlagsSet() {
+        ReceiptPrinterHandler printerHandler = new ReceiptPrinterHandler(null);
+        printerHandler.thePrinterIsOutOfPaper();
+        printerHandler.thePrinterIsOutOfInk();
+        assertTrue(printerHandler.refillFlagsSet());
+    }
+
+    @Test
+    public void testRefillFlagsSetWhenFlagsResetAfterRefill() {
+        ReceiptPrinterHandler printerHandler = new ReceiptPrinterHandler(null);
+        printerHandler.thePrinterIsOutOfPaper();
+        printerHandler.thePrinterIsOutOfInk();
+        assertTrue(printerHandler.refillFlagsSet());
+        printerHandler.paperHasBeenAddedToThePrinter();
+        assertTrue(printerHandler.refillFlagsSet());
+        printerHandler.inkHasBeenAddedToThePrinter();
+        assertFalse(printerHandler.refillFlagsSet());
+    }
+}
