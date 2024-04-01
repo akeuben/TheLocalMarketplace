28f0fe3916973ff60c55c218fe91da6d043c758b
Ammaar Melethil <100314064+ammaarmelethil@users.noreply.github.com>
2024-03-31 19:49:07 -0600

    Update MaintainBanknotes.java

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/attendant/MaintainBanknotes.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/attendant/MaintainBanknotes.java
@@ -26,13 +26,20 @@ package com.thelocalmarketplace.software.attendant;
  * Winston Wang - 30185321
  */
 
+import java.util.List;
+
+import com.tdc.CashOverloadException;
+import com.tdc.banknote.Banknote;
+
+
 import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
 
+import ca.ucalgary.seng300.simulation.InvalidStateSimulationException;
+import ca.ucalgary.seng300.simulation.SimulationException;
 
 
 public class MaintainBanknotes {
 	private AbstractSelfCheckoutStation station;
-	
 	
 	/**
 	 * Constructor
@@ -43,6 +50,60 @@ public class MaintainBanknotes {
 		this.station = station;
 	}
 	
+	/**
+	 * Allows the attendant to add bank notes
+	 * @param banknotes
+	 * 		can accept multiple banknotes
+	 * @throws SimulationException
+	 * @throws CashOverloadException
+	 */
+	public void addBanknotes(Banknote... banknotes) throws SimulationException, CashOverloadException {
+		
+		// Check if there is enough space to add the bank note(s)
+		if (banknotes.length > (station.getBanknoteStorage().getCapacity() - station.getBanknoteStorage().getBanknoteCount())) {
+			throw new InvalidStateSimulationException("Not enough space to add banknotes");
+		}
+		
+		// Check if the station is disabled, then proceed to add bank note(s)
+		if (station.getBanknoteStorage().isDisabled()) {
+			station.getBanknoteStorage().load(banknotes);
+		} else {
+			throw new InvalidStateSimulationException("Disable the station before adding banknotes");
+		}	
+	}
 	
+	/**
+	 * Allows the attendant to remove bank notes
+	 * @param quantityToRemove
+	 * @return
+	 * 		List of bank notes that were removed
+	 * @throws SimulationException
+	 * @throws CashOverloadException
+	 */
+	public List<Banknote> removeBanknotes(int quantityToRemove) throws SimulationException, CashOverloadException {
+		List<Banknote> removed;
+		
+		// First Check if there are enough bank notes to remove 
+		if (quantityToRemove > station.getBanknoteStorage().getBanknoteCount()) {
+			throw new InvalidStateSimulationException("Requested quantity to remove exceeds current quantity");
+		}
+		
+		// Check if station is disabled then proceed to remove bank notes
+		if ((station.getBanknoteStorage().isDisabled()) && (station.getBanknoteStorage().getBanknoteCount() >= quantityToRemove)) {
+			// Unload all bank notes then add back ones that need not be removed
+			removed = station.getBanknoteStorage().unload();
+			int banknotesToReturn = removed.size() - quantityToRemove;
+			
+			for (int i=0; i<banknotesToReturn; i++) {
+				station.getBanknoteStorage().load(removed.get(removed.size() - 1));
+				removed.remove(removed.size() - 1);
+			}
+		} else {
+			throw new InvalidStateSimulationException("Disable the station before removing banknotes");
+		}
 	
+		return removed;
+	}
+	
+
 }
