cf8e72c09a75fb2e5a9c3a0897c83ed763a81ef2
Joey - School <joey.fisher@ucalgar.ca>
2024-04-04 08:45:13 -0600

    Implemented the state for adding own bags. Adding own bag increases the expected mass of the scale by the ideal mass of a reusable bag. There is a new UIObserver for doneAddingBagsSelected. This must be interacted with before state = waiting for bagging then verifies the new weight with the bag.

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/payment/Transaction.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/payment/Transaction.java
@@ -65,6 +65,7 @@ public class Transaction {
 		if (product != null) {
 			products.add(product);
 			totalCost = totalCost.add(BigDecimal.valueOf(product.getPrice()).divide(BigDecimal.valueOf(100)));
+			expectedMass = expectedMass.sum(new Mass(BigInteger.valueOf((int) (product.getExpectedWeight()*Mass.MICROGRAMS_PER_GRAM))));
 		} else {
 				throw new NullPointerException("product");
 		}
