cf78a437e3b8463f1afb52208dbc88b219db50e4
NeerajGHANSELA <160438830+NeerajGHANSELA@users.noreply.github.com>
2024-04-06 23:15:39 -0600

    Create MembershipTest.java

---
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/MembershipTest.java
@@ -0,0 +1,45 @@
+package com.thelocalmarketplace.software.test;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+
+public class MembershipTest {
+
+    Membership my_Membership;
+  
+    @Before
+    public void setup() {
+        my_Membership = new Membership("joe", 1, 100);
+    }
+    
+    @Test
+    public void TestConstructor() {
+        assertEquals(my_Membership.getMemberName(), "joe");
+        assertEquals(my_Membership.getMembershipNumber(), 1);
+        assertEquals(my_Membership.getMemberPoints(), 100);
+    }
+
+    @Test
+    public void TestgetMemberName() {
+        assertEquals(my_Membership.getMemberName(), "joe");
+    }
+
+    @Test
+    public void TestgetMembershipNumber() {
+        assertEquals(my_Membership.getMembershipNumber(), 1);
+    }
+
+    @Test
+    public void TestgetMemberPoints() {
+        assertEquals(my_Membership.getMemberPoints(), 100);
+    }
+
+    @Test
+    public void TestchangeMemberPoints() {
+        my_Membership.changeMemberPoints(1000);
+        assertEquals(my_Membership.getMemberPoints(), 1100);
+    }
+
+}
