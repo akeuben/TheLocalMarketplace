3ece1b577641425f90812843b421ff4374ce6e36
samt25 <turnersr@ucalgary.ca>
2024-04-06 21:26:53 -0600

    error fixes

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/state/ReadyForItemState.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/state/ReadyForItemState.java
@@ -83,7 +83,6 @@ public class ReadyForItemState implements IUserSessionState<UserSessionState> {
 	@Override
 	public UserSessionState onPLUentered(UserSession session, PriceLookUpCode plu) {;
 		PLUCodedProduct product = ProductDatabases.PLU_PRODUCT_DATABASE.get(plu);
-		int checkoutID = 0;
 		IElectronicScale scale = session.getHardware().getScanningArea();
 		Mass massOnScale;
 		try {
@@ -92,7 +91,7 @@ public class ReadyForItemState implements IUserSessionState<UserSessionState> {
 			throw new RuntimeException("The scale is currently overloaded.");
 		}
 		if (product!=null) {
-			Software.getInstance().getCurrentSession(checkoutID).getTransaction().addItem(product, massOnScale);
+			session.getTransaction().addItem(product, massOnScale);
 			return UserSessionState.WAITING_FOR_BAGGING;
 		}
 		return null;
